992d77d88c60a4464e9a7193cf2b588c580ce85c
Lucian Lodico <llodico@mac.com>
2022-11-07 09:50:11 -0500

    show/hide - will add new css

--- freeshelf/src/App.js
+++ freeshelf/src/App.js
@@ -9,6 +9,13 @@ const App = ({ books }) => {
           <div>
             <h2>{books.title}</h2>
             <h3>{books.author}</h3>
+            <p>{books.shortDescription}</p>
+            <img src={books.coverImageUrl} className="image" alt="cover image" /> 
+            <XtraInfo
+            url={books.url}
+            publisher={books.publisher}
+            date={books.publicationDate}
+            details={books.detailedDescription} />
           </div>
         </div>
       ))
@@ -16,5 +23,24 @@ const App = ({ books }) => {
     </section>
   );
 }
+
+const XtraInfo = ({url, publisher, date, details }) => {
+  const [readMore, setreadMore] = useState(false)
+  const handleClick = (event) => {setreadMore(!readMore)}
+  return (
+      <>
+    <button onClick={handleClick} 
+      type="button" aria-controls="det" aria-expanded={readMore ? "true" : "false"}>
+      <strong>{readMore ? 'Hide' : 'Show'} More </strong></button>
+    {readMore && 
+    <div id="det" className="description-box">
+    <strong>Link:</strong> <a href={url}>{url}</a>
+    <p><strong>Full Description:</strong><br/>{details}</p>
+    <p><strong>Publisher:</strong> {publisher ? <>{publisher}</> : "--"}  <br/><br/> 
+    <strong>Publication Date:</strong> {date ? <>{date}</> : "--"}</p>
+  </div> 
+  }
+  </> 
+  )}; 
 
 export default App;
